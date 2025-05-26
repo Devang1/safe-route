c9a676f20d55e0ce4b11a144c6029cfdbd5ef1da
Devang1 <115930424+Devang1@users.noreply.github.com>
2025-05-26 22:43:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server:{
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: "https://safe-route-nm6k.onrender.com"||'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       },
